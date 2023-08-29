536eaf3c1c59b15596f72205df791f2469444bf0
MmmarRTHa <m27842n@outlook.com>
2023-08-28 22:29:36 -0700

    first commit

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,11 @@
+
+// rsc to create stateless component
+const CustomerReviews = () => {
+    return (
+        <div>
+            CustomerReviews
+        </div>
+    );
+};
+
+export default CustomerReviews;
